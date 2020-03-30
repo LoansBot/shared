e8322fb50da988a433131f37dc2541da55788e17
Timothy Moore <mtimothy984@gmail.com>
2020-03-30 07:03:44 -0700

    Setup project structure, integrations

---
+++ README.md
@@ -0,0 +1,7 @@
+# LoansBot - Shared
+
+This repository contains Python files that are used in at least two LoansBot
+repositories but aren't large enough and/or coupled enough to warrant their
+own repository.
+
+To avoid naming conflicts the main module is "lbshared" instead of shared.
---
+++ tests/test_integrations.py
@@ -0,0 +1,58 @@
+"""Verifies that all the connections that can be created using the integrations
+module are able to service simple requests"""
+import unittest
+import sys
+import secrets
+
+sys.path.append("../src")
+
+import lbshared.integrations  # noqa: E402
+
+
+class TestIntegrations(unittest.TestCase):
+    def test_database(self):
+        conn = lbshared.integrations.database()
+        try:
+            cursor = conn.cursor()
+            cursor.execute('SELECT NOW()')
+            row = cursor.fetchone()
+            self.assertIsNotNone(row)
+            self.assertEqual(len(row), 1)
+            cursor.close()
+        finally:
+            conn.close()
+
+    def test_amqp(self):
+        amqp = lbshared.integrations.amqp()
+        try:
+            channel = amqp.channel()
+            channel.queue_declare('test_integrations')
+            pub_body = secrets.token_urlsafe(16).encode('utf-8')
+            channel.basic_publish(
+                exchange='', routing_key='test_integrations', body=pub_body
+            )
+            for mf, props, body in channel.consume('test_integrations', inactivity_timeout=5):
+                self.assertIsNotNone(mf)
+                channel.basic_ack(mf.delivery_tag)
+                channel.cancel()
+                self.assertEqual(body, pub_body)
+                break
+            channel.close()
+        finally:
+            amqp.close()
+
+    def test_cache(self):
+        client = lbshared.integrations.cache()
+        try:
+            key = 'test_integrations'
+            val = secrets.token_urlsafe(16).encode('utf-8')
+            client.set(key, val, expire=1)
+            self.assertEqual(client.get(key), val)
+            client.delete(key)
+            self.assertIsNone(client.get(key))
+        finally:
+            client.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
