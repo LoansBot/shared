89ba7fe3affa93c8b8ba3b2c37a7f18a073377ba
Timothy Moore <mtimothy984@gmail.com>
2020-03-31 06:21:56 -0700

    Add a retry helper (#4)
    This is an ultra simple way to make each program supervisor nice about restarts (and hence not consume too many resources while a required integration is down)

---
+++ src/lbshared/retry_helper.py
@@ -0,0 +1,42 @@
+"""
+A small module that will use a single file to store when the program was last
+booted and, if it was too recent, will sleep a little bit. Typically called
+at the start of the program.
+"""
+import os
+import json
+import time
+
+
+FILENAME = '.retry_helper'
+
+
+def handle(time_between_restarts=10):
+    """This will store when this application last opened in FILENAME and
+    sleep as necessary to ensure that when this file exits it's been at
+    least the given time between restarts since the last time this function
+    was called"""
+    last_open_at = last_opened_at()
+    store_opened_at()
+
+    if last_open_at is not None:
+        time_since_opened = time.time() - last_open_at
+        if time_since_opened < time_between_restarts:
+            time.sleep(time_between_restarts - time_since_opened)
+
+
+def last_opened_at():
+    """Determines when store_opened_at was last called"""
+    if not os.path.exists(FILENAME):
+        return None
+
+    with open(FILENAME, 'r') as f:
+        body = json.load(f)
+
+    return body['last_started_at']
+
+
+def store_opened_at():
+    """Updates the retry helper file to indicate we were just run"""
+    with open(FILENAME, 'w') as f:
+        json.dump({'last_started_at': time.time()}, f)
