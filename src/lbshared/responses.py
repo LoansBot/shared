1b925adad456522bb49440a391801d3c115a74d3
Timothy Moore <mtimothy984@gmail.com>
2020-04-01 09:03:56 -0700

    Add response formatting utility (#7)
    This adds two convenience functions for formatting responses - one for getting a single response and one for getting two responses which are in the standard title/body split. These support a different format that LoansBotv1 in that where before a response might look like
    
    Here is my information on /u/<user1>:
    
    Now it looks like
    
    Here is my information on /u/{user1}:

---
+++ src/lbshared/responses.py
@@ -0,0 +1,89 @@
+"""A collection of convenience functions for formatting responses."""
+from .lazy_integrations import LazyIntegrations
+from lblogging import Level
+from pypika import PostgreSQLQuery as Query, Table, Parameter
+import traceback
+
+
+class DefaultDictWithKeyArg(dict):
+    """Essentially a default dictionary, except it passes the name of the
+    key to the __missing__ lambda."""
+    def __init__(self, default):
+        self.default = default
+
+    def __missing__(self, key):
+        return self.default(key)
+
+
+def get_response(itgs: LazyIntegrations, name: str, **replacements):
+    """Get the formatted response with the given name using the given
+    substitutions. Every substitution is formatted using str(), if replacements
+    is missing any keys the response expects the event will be logged but a
+    response will be returned. If replacements contains keys not expected
+    by the response they are simply ignored.
+
+    @param [LazyIntegrations] itgs The lazy integrations to use for connecting
+        to the database and/or logger. This will only use a read cursor on the
+        database.
+    @param [str] name The name of the response to format
+    @param [dict] replacements A map from the names of keys to substitute within
+      the response body to an object which will be stringified.
+    @return [str] The formatted response.
+    """
+    responses = Table('responses')
+    itgs.read_cursor.execute(
+        Query.from_(responses).select(responses.response_body)
+        .where(responses.name == Parameter('%s')).limit(1).get_sql(),
+        (name,)
+    )
+    row = itgs.read_cursor.fetchone()
+    if row is None:
+        itgs.logger.print(
+            Level.WARN,
+            'There was a request to format the response {} which is not a '
+            'response name for which a response format exists! '
+            'Stack trace:\n{}',
+            name, ''.join(traceback.format_stack())
+        )
+        return f'ERROR: Unknown response: "{name}"'
+    (unformatted,) = row
+
+    def factory(key):
+        itgs.logger.print(
+            Level.WARN,
+            'While formatting response {} there was a request to substitute {} '
+            'but the only known substitutions are {}',
+            name, key, ', '.join(replacements.keys())
+        )
+        return f'[ERROR: unknown substitution "{key}"]'
+
+    format_dict = DefaultDictWithKeyArg(factory)
+    for k, v in replacements.items():
+        format_dict[k] = v
+    return unformatted.format_map(format_dict)
+
+
+def get_letter_response(itgs: LazyIntegrations, base_name: str, **replacements):
+    """This is a helper method for formatting "letter responses", which are
+    responses which have a title and body and the same substitutions are used
+    for both. In practice some of the replacements for the body might look
+    silly in the title, but it doesn't hurt to expose them there.
+
+    This assumes that the response names are `f'{base_name}_title'` and
+    `f'{base_name}_body'` respectively.
+
+    @param [LazyIntegrations] itgs The lazy integrations to use for connecting
+        to the database and/or logger. This will only use a read cursor on the
+        database.
+    @param [str] base_name The base of the response names for the title and
+        body. Suffixed with _title and _body for the title and body
+        respectively.
+    @param [dict] replacements A dictionary where keys are the names of keys
+        in the response to substitute, as if by format_map.
+    @return [str, str] Two tuples; the first is the formatted title and the
+        second is the formatted body.
+    """
+    return (
+        get_response(itgs, base_name + '_title', **replacements),
+        get_response(itgs, base_name + '_body', **replacements)
+    )
---
+++ tests/test_responses.py
@@ -0,0 +1,60 @@
+"""Verifies that all the connections that can be created using the integrations
+module are able to service simple requests"""
+import unittest
+import sys
+from pypika import PostgreSQLQuery as Query, Table, Parameter
+
+sys.path.append("../src")
+
+import lbshared.responses as responses  # noqa: E402
+from lbshared.lazy_integrations import LazyIntegrations  # noqa: E402
+
+
+class TestResponses(unittest.TestCase):
+    def test_missing(self):
+        with LazyIntegrations() as itgs:
+            res = responses.get_response(itgs, 'my_missing_key')
+            self.assertIsInstance(res, str)
+            self.assertIn('my_missing_key', res)
+
+    def test_existing(self):
+        resps = Table('responses')
+        with LazyIntegrations() as itgs:
+            itgs.write_cursor.execute(
+                Query.into(resps).columns(
+                    resps.name,
+                    resps.response_body,
+                    resps.description
+                ).insert(*[Parameter('%s') for _ in range(3)])
+                .returning(resps.id).get_sql(),
+                (
+                    'my_response',
+                    'I like to {foo} the {bar}',
+                    'Testing desc'
+                )
+            )
+            (respid,) = itgs.write_cursor.fetchone()
+            try:
+                itgs.write_conn.commit()
+                res: str = responses.get_response(itgs, 'my_response', foo='open', bar='door')
+                self.assertEqual(res, 'I like to open the door')
+
+                res: str = responses.get_response(itgs, 'my_response', foo='eat', buzz='bear')
+                self.assertIsInstance(res, str)
+                self.assertTrue(res.startswith('I like to eat the '), res)
+                # it's not important how we choose to format the error, but it
+                # needs the missing key or debugging will be a pain
+                self.assertIn('bar', res)
+            finally:
+                itgs.write_conn.rollback()
+                itgs.write_cursor.execute(
+                    Query.from_(resps).delete()
+                    .where(resps.id == Parameter('%s'))
+                    .get_sql(),
+                    (respid,)
+                )
+                itgs.write_conn.commit()
+
+
+if __name__ == '__main__':
+    unittest.main()
