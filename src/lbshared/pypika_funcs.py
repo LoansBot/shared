081caaf8290aebded44796530b2ebc12bcbe2430
Timothy Moore <mtimothy984@gmail.com>
2020-10-17 08:16:21 -0700

    Add DATE_PART function

--- src/lbshared/pypika_funcs.py
+++ src/lbshared/pypika_funcs.py
@@ -10,3 +10,8 @@ class Greatest(Function):
 class DateTrunc(Function):
     def __init__(self, *args, alias=None):
         super().__init__('DATE_TRUNC', *args, alias=alias)
+
+
+class DatePart(Function):
+    def __init__(self, part, expr):
+        super(DatePart, self).__init__('DATE_PART', part, expr)
